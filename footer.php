48c2dee1e2786a385816d3742a79bf31efb19ccc
sjapaget <sjapaget@gmail.com>
2022-05-24 09:39:29 +0200

    center elements on footer

--- footer.php
+++ footer.php
@@ -25,11 +25,12 @@
 				printf( esc_html__( 'Proudly powered by %s', 'ridiy' ), 'WordPress' );
 				?>
 			</a>
-			<span class="sep"> | </span>
+			<div>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'ridiy' ), 'ridiy', '<a href="https://github.com/sjapaget/wp-theme">Samuel Paget</a>' );
 				?>
+			</div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
